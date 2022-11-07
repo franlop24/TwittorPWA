19e17ea2cb630c68c2a7ac83efaa0453987427b0
franlops <franlopbri@gmail.com>
2022-11-07 13:55:26 -0600

    Cache with network fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const STATIC_CACHE = 'static-v1';
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -12,7 +14,8 @@ const APP_SHELL = [
     '/img/avatars/spiderman.jpg',
     '/img/avatars/thor.jpg',
     '/img/avatars/wolverine.jpg',
-    '/js/app.js'
+    '/js/app.js',
+    '/js/sw-utils.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -39,4 +42,18 @@ self.addEventListener('activate', e => {
     });
 
     e.waitUntil(respuesta);
+})
+
+self.addEventListener('fetch', e => {
+    const respuesta = caches.match(e.request).then(resp => {
+         if(resp){
+            return resp;
+         } else {
+            return fetch(e.request).then(newRes => {
+                return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+            })
+         }
+    });
+
+    e.respondWith(respuesta);
 })
